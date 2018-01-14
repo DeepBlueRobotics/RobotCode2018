1a0fee0b54047e63d2c63122c85097a4ae52609b
lhmcgann <hailey.mcgann@gmail.com>
2018-01-13 16:15:11 -0800

    added subsystems and interfaces
    subsystems and interfaces: Lift, Climber, ClimberAssist, IntakeEject
    added "implements (interface) in each subsystem
    did not include drivetrain or auto bc other peopl already doing that

---
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/subsystems/IntakeEject.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team199.Robot2018.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class IntakeEject extends Subsystem implements IntakeEjectInterface {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
---
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/subsystems/LiftInterface.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team199.Robot2018.subsystems;
+
+public interface LiftInterface {
+
+}
