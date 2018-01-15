3b9e39ae92ba5d56f082bf8ed3120b33510184ba
Alex Binnard <a.binnard@gmail.com>
2018-01-15 14:36:16 -0800

    ParseScriptFile actually terminates and I wrote my javadoc.

--- Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/ParseScriptFile.java
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/ParseScriptFile.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import javax.naming.LinkLoopException;
 import javax.security.auth.login.FailedLoginException;
 
 import org.usfirst.frc.team199.Robot2018.Robot;
@@ -18,6 +19,8 @@ import edu.wpi.first.wpilibj.hal.ThreadsJNI;
  */
 public class ParseScriptFile extends Command {
 
+	private boolean returnThis = false;
+	
     public ParseScriptFile() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -30,16 +33,25 @@ public class ParseScriptFile extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 		String scriptFile = Preferences.getInstance().getString("autoscripts", "aaaaaaa");
-		ArrayList<String> lines = new ArrayList<String>(Arrays.asList(scriptFile.split("\\r?\\n")));
+		ArrayList<String> lines = new ArrayList<String>(Arrays.asList(scriptFile.split("\\r?\\n"))); //splits the input from Preferences into lines and makes an ArrayList
 		boolean finished = false;
 		ArrayList<ArrayList<String>> returned;
-		while (!finished) {
+		while (!finished) { //loops until we run out of colon lines
 			returned = splitAtSecondColon(lines);
 			finished = returned.get(1)==null;
 			lines = returned.get(1);
-			Robot.autoScripts.put(returned.get(0).get(0).substring(0, returned.get(0).get(0).length()-1), returned.get(0));
+			Robot.autoScripts.put(returned.get(0).get(0).substring(0, returned.get(0).get(0).length()-1), returned.get(0)); //sending scripts to Robot.java's hashmap
 		}
+		returnThis = true; //command is finished
     }
+    
+    /*
+     * Takes an ArrayLisfalset of Strings and splits it into two ArrayLists;
+     * the part before the second string with a colon, and the rest.
+     * They are returned as an ArrayList of ArrayLists of Strings.
+     * @param lines the ArrayList to be split
+     * @return an ArrayList with two ArrayLists of Strings. Index 0 is before the split, index 1 is after. Index 1 will be null if there is only one colon.
+     */
 	private ArrayList<ArrayList<String>> splitAtSecondColon(ArrayList<String> lines) {
 		ArrayList<String> current = new ArrayList<String>();
 		boolean finished = true;
@@ -52,7 +64,7 @@ public class ParseScriptFile extends Command {
 				finished = false;
 			}
 		}
-		if (finished) {
+		if (finished) { //runs if the script doesn't split
 			current = lines;
 			lines = null;
 		}
@@ -63,7 +75,7 @@ public class ParseScriptFile extends Command {
 	}
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return returnThis;
     }
 
     // Called once after isFinished returns true
