ae7b61bb3bf8aede304fc9b3a6d9f34275cb0ab2
Alex Binnard <a.binnard@gmail.com>
2018-01-15 08:38:23 -0800

    ugh updating made merge errors

--- Robot2018/src/tests/scriptupload/scriptupload.py
+++ Robot2018/src/tests/scriptupload/scriptupload.py
@@ -6,26 +6,24 @@ prefs = NetworkTables.getTable("Preferences")
 
 go = False #true when file has been successfully read
 lines = [] #an array of strings which represent each line of the file
-filename = input("File name: ") #the name of the file to read (user input)
+filename = raw_input("File name: ") #the name of the file to read (user input)
 oneline = "" #the file as a single string
+retry = False
 
 #loops until a file is read into the file array
 while not go:
-    try:
-        if filename == "quit":
+    if retry:
+        filename = raw_input("Not found. Try another name (enter to quit): ")#retry, or quit (in case the file doesn't exist)
+        if filename == "":
             quit()
-            
+    try:
         with open(filename) as script:
             lines = script.readlines()
-            "".join(lines)
-            #balex's testing
-            #for line in file:
-             #   print(repr(line))
-            
+            "".join(lines) 
         break
     except:
-        filename = input("Not found. Try another name (enter to quit): ") or "quit" #retry, or quit (in case the file doesn't exist)
-
+        retry = True
 #puts the string array
 prefs.putString("autoscripts", file)
+prefs.putStringArray("autoscripts", file)
 print("Uploaded %s as a String[] to key \"autoscripts\"" % filename)
