88efe6302d922ea39864933ab4acba4d08f1eb99
doawelul <dfang314@gmail.com>
2018-01-18 21:12:58 -0800

    added end() to the interrupted() in the commands as per request

--- Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/ShiftHighGear.java
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/ShiftHighGear.java
@@ -44,5 +44,6 @@ public class ShiftHighGear extends Command {
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
