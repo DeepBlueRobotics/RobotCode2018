8ca27e3ea5be3848818b335da97cd79ff12a3ef9
Alex Binnard <a.binnard@gmail.com>
2018-02-10 16:30:43 -0800

    fix my horrible redunant code and Corvin's test

--- Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/AutoMoveTo.java
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/AutoMoveTo.java
@@ -50,35 +50,6 @@ public class AutoMoveTo extends CommandGroup {
 	}
 
 	public AutoMoveTo(String[] args) {
-		// requires(Drivetrain);
-		double rotation;
-		double[] point = { 0, 0 };
-		for (String arg : args) {
-			if (AutoUtils.isDouble(arg)) {
-				rotation = Double.valueOf(arg);
-				double relrotation = rotation - AutoUtils.position.getRot();
-				addSequential(new PIDTurn(relrotation, Robot.dt, Robot.sd, Robot.dt.getDistEncAvg()));
-				AutoUtils.position.setRot(rotation);
-			} else if (AutoUtils.isPoint(arg)) {
-				point = AutoUtils.parsePoint(arg);
-				double deltaX = point[0] - AutoUtils.position.getX();
-				double deltaY = point[1] - AutoUtils.position.getY();
-				double atan = Math.toDegrees(Math.atan(deltaX / deltaY));
-				double relrotation = atan - AutoUtils.position.getRot();
-				addSequential(new PIDTurn(relrotation, Robot.dt, Robot.sd, Robot.dt.getDistEncAvg()));
-
-				double dX2 = deltaX * deltaX;
-				double dY2 = deltaY * deltaY;
-				double distance = Math.sqrt(dX2 + dY2);
-				addSequential(new PIDMove(distance, Robot.dt, Robot.sd, Robot.dt.getDistEncAvg()));
-				double x = AutoUtils.position.getX();
-				double y = AutoUtils.position.getY();
-				AutoUtils.position.setX(point[0]);
-				AutoUtils.position.setY(point[1]);
-				AutoUtils.position.setRot(Math.toDegrees(Math.atan((point[0] - x) / (point[1] - y))));
-			} else {
-				throw new IllegalArgumentException();
-			}
-		}
+		this(args, Robot.dt, Robot.sd, Robot.dt.getDistEncAvg());
 	}
 }
--- Robot2018/test/org/usfirst/frc/team199/Robot2018/AutoMoveToTest.java
+++ Robot2018/test/org/usfirst/frc/team199/Robot2018/AutoMoveToTest.java
@@ -1,40 +1,30 @@
 package org.usfirst.frc.team199.Robot2018;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.usfirst.frc.team199.Robot2018.autonomous.AutoUtils;
+import org.usfirst.frc.team199.Robot2018.commands.AutoMoveTo;
+import org.usfirst.frc.team199.Robot2018.subsystems.DrivetrainInterface;
 
-import static org.mockito.Mockito.*;
-
+import edu.wpi.first.wpilibj.HLUsageReporting;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.internal.HardwareTimer;
-import edu.wpi.first.wpilibj.HLUsageReporting;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDSource;
-import edu.wpi.first.wpilibj.PIDSourceType;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-
-import com.kauailabs.navx.frc.AHRS;
-
-import org.usfirst.frc.team199.Robot2018.commands.Autonomous;
-import org.usfirst.frc.team199.Robot2018.commands.TeleopDrive;
-import org.usfirst.frc.team199.Robot2018.commands.Autonomous.Position;
-import org.usfirst.frc.team199.Robot2018.commands.Autonomous.Strategy;
-import org.usfirst.frc.team199.Robot2018.subsystems.DrivetrainInterface;
-import org.usfirst.frc.team199.Robot2018.autonomous.AutoUtils;
-import org.usfirst.frc.team199.Robot2018.commands.AutoMoveTo;
-
-import java.util.HashMap;
-import java.util.Map;
 
 class AutoMoveToTest {
 
-	// May need a BeforeEach to reset AutoUtils.
+	// May need a BeforeEach to reset AutoUtils
 
 	@BeforeEach
 	void setUp() {
@@ -48,90 +38,96 @@ class AutoMoveToTest {
 		HLUsageReporting.Interface usageReporter = mock(HLUsageReporting.Interface.class);
 		HLUsageReporting.SetImplementation(usageReporter);
 	}
-	
+
 	@Test
 	void testWPICommand() {
 		Command command = new Command() {
-			protected boolean isFinished() { return false; }			
+			protected boolean isFinished() {
+				return false;
+			}
 		};
 		assertNotNull(command);
 	}
-	
+
 	@Test
 	void testWPICommandGroup() {
 		CommandGroup group = new CommandGroup();
 		assertNotNull(group);
 
 		Command command = new Command() {
-			protected boolean isFinished() { return false; }			
+			protected boolean isFinished() {
+				return false;
+			}
 		};
 		group.addSequential(command);
 	}
-	
+
 	@Test
 	void testPIDController() {
 		PIDSource source = mock(PIDSource.class);
-		PIDOutput output = mock(PIDOutput.class);		
-		
+		PIDOutput output = mock(PIDOutput.class);
+
 		PIDController ctrl = new PIDController(0, 0, 0, source, output);
 		assertNotNull(ctrl);
 	}
-	
-	//@Test
+
+	// @Test
 	// Problem instantiating Subsystem because of SendableBase using network tables.
 	void testWPISubsystem() {
-		//LiveWindow.setEnabled(false);
+		// LiveWindow.setEnabled(false);
 		Subsystem subsystem = new Subsystem() {
 			protected void initDefaultCommand() {
 				setDefaultCommand(new Command() {
-					protected boolean isFinished() { return false; }			
+					protected boolean isFinished() {
+						return false;
+					}
 				});
 			}
 		};
 		assertNotNull(subsystem);
 	}
-		
+
 	@Test
 	void testForwardAndRight() {
-		String[] args = {"(0,12)","(12,12)"};
-		
-		AutoUtils.setRot(0);
-		AutoUtils.setX(0);
-		AutoUtils.setY(0);
-		
+		String[] args = { "(0,12)", "(12,12)" };
+
+		AutoUtils.position.setRot(0);
+		AutoUtils.position.setX(0);
+		AutoUtils.position.setY(0);
+
 		PIDSource pidGyroSrc = mock(PIDSource.class);
 		when(pidGyroSrc.getPIDSourceType()).thenReturn(PIDSourceType.kDisplacement);
 		DrivetrainInterface dt = mock(DrivetrainInterface.class);
-		when(dt.getGyro()).thenReturn(pidGyroSrc);		
+		when(dt.getGyro()).thenReturn(pidGyroSrc);
 		SmartDashboardInterface sd = mock(SmartDashboardInterface.class);
 		PIDSource pidMoveSrc = mock(PIDSource.class);
-		
+
 		AutoMoveTo testAMT = new AutoMoveTo(args, dt, sd, pidMoveSrc);
-		
-		assertEquals(90, AutoUtils.getRot());
-		assertEquals(12, AutoUtils.getX());
-		assertEquals(12, AutoUtils.getY());
+
+		assertEquals(90, AutoUtils.position.getRot());
+		assertEquals(12, AutoUtils.position.getX());
+		assertEquals(12, AutoUtils.position.getY());
 	}
 
 	@Test
 	void testForward() {
-		String[] args = {"(0,12)"};
-		
-		AutoUtils.setRot(0);
-		AutoUtils.setX(0);
-		AutoUtils.setY(0);
-		
+		String[] args = { "(0,12)" };
+
+		AutoUtils.position.setRot(0);
+		AutoUtils.position.setX(0);
+		AutoUtils.position.setY(0);
+
 		PIDSource pidGyroSrc = mock(PIDSource.class);
 		when(pidGyroSrc.getPIDSourceType()).thenReturn(PIDSourceType.kDisplacement);
 		DrivetrainInterface dt = mock(DrivetrainInterface.class);
-		when(dt.getGyro()).thenReturn(pidGyroSrc);		
+		when(dt.getGyro()).thenReturn(pidGyroSrc);
 		SmartDashboardInterface sd = mock(SmartDashboardInterface.class);
 		PIDSource pidMoveSrc = mock(PIDSource.class);
-		
+
 		AutoMoveTo testAMT = new AutoMoveTo(args, dt, sd, pidMoveSrc);
-		
-		assertEquals(0, AutoUtils.getRot());
-		assertEquals(0, AutoUtils.getX());
-		assertEquals(12, AutoUtils.getY());
+
+		assertEquals(0, AutoUtils.position.getRot());
+		assertEquals(0, AutoUtils.position.getX());
+		assertEquals(12, AutoUtils.position.getY());
 	}
 }
