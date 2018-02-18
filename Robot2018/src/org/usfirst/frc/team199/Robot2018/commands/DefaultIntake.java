d6bfa17b4c04a959ba3a4f031b77ca61c99e9da8
doawelul <dfang314@gmail.com>
2018-02-18 14:18:37 -0800

    add end and interrupted to defaultIntake

--- Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/DefaultIntake.java
+++ Robot2018/src/org/usfirst/frc/team199/Robot2018/commands/DefaultIntake.java
@@ -33,10 +33,12 @@ public class DefaultIntake extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
+		Robot.intakeEject.runIntake(0);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
